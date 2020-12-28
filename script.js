4288e2c6a5a5fbf16f2de52487347102216d3969
Ajaydip Singh <ajaylottey@gmail.com>
2020-12-28 22:41:01 +0300

    :sparkles: feat: add door opening logic

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-let doorImage1 = document.getElementById('door1');
-
 let botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
+let beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
+let spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg"
 
+let doorImage1 = document.getElementById('door1');
 doorImage1.onclick = () => {
     doorImage1.src = botDoorPath;
 }
 
 let doorImage2 = document.getElementById('door2');
-
 doorImage2.onclick = () => {
-    doorImage2.src = botDoorPath;
+    doorImage2.src = beachDoorPath;
 }
 
 let doorImage3 = document.getElementById('door3');
-
 doorImage3.onclick = () => {
-    doorImage3.src = botDoorPath;
+    doorImage3.src = spaceDoorPath;
 }
 
 
