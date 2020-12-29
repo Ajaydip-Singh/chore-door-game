ae9148e0279b331f02d764fb0ac89ace1cc42bde
Ajaydip Singh <ajaylottey@gmail.com>
2020-12-29 22:58:24 +0300

    :sparkles: feat: add random door logic

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 let botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
 let beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
-let spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg"
+let spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
+
+
+let numClosedDoors = 3;
+
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = () => {
+    const choreDoor = Math.floor(Math.random() * numClosedDoors);
+
+    if (choreDoor == 0) {
+        openDoor1 = botDoorPath;
+        openDoor2 = beachDoorPath;
+        openDoor3 = spaceDoorPath;
+    } else if (choreDoor == 1) {
+        openDoor1 = beachDoorPath;
+        openDoor2 = botDoorPath;
+        openDoor3 = spaceDoorPath;
+    } else {
+        openDoor1 = beachDoorPath;
+        openDoor2 = spaceDoorPath;
+        openDoor3 = botDoorPath;
+    }
+};
+
+randomChoreDoorGenerator();
 
 let doorImage1 = document.getElementById('door1');
 doorImage1.onclick = () => {
-    doorImage1.src = botDoorPath;
-}
+    doorImage1.src = openDoor1;
+};
 
 let doorImage2 = document.getElementById('door2');
 doorImage2.onclick = () => {
-    doorImage2.src = beachDoorPath;
-}
+    doorImage2.src = openDoor2;
+};
 
 let doorImage3 = document.getElementById('door3');
 doorImage3.onclick = () => {
-    doorImage3.src = spaceDoorPath;
-}
-
-
-
-
+    doorImage3.src = openDoor3;
+};
